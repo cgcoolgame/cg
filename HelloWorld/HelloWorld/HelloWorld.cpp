172c24a10bec8e5ef881593e2177a247b81f249c
lieyuconglin <huangyuliadao@163.com>
2015-07-29 23:54:11 +0800

    HelloWorld

--- HelloWorld/HelloWorld/HelloWorld.cpp
+++ HelloWorld/HelloWorld/HelloWorld.cpp
@@ -2,10 +2,34 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+#ifdef _DEBUG
+template<typename T>
+T TMax(T l, T r)
+{
+	return l > r?l:r;
+}
+#else
+#define TMax(a,b) ((a)>(b)?(a):(b))
+#endif
 
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	int nFind = 0;
+	for (int i = 0; i < 100; ++i)
+	{
+		cout<<"Hello World !"<<endl;
+
+		nFind = TMax(nFind, rand());
+	}
+
+	cout<<"Max = "<<nFind<<endl;
+
+	system("pause");
+
 	return 0;
 }
 
